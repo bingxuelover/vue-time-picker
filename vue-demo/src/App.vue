fd278d6372e4f021b1fafd069a4a3deea07b7804
huangyuhong <yuhong0119@163.com>
2019-12-06 11:14:20 +0800

    vue version

---
+++ vue-demo/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <TimePicker
+      :getOptions="options"
+      :timeOccupied="timeOccupied"
+      :timeRange="timeRange"
+      :defaultStart="defaultStart"
+      :defaultEnd="defaultEnd"
+      @change-time="changeTime"
+    />
+    <TimeRangePicker
+      :timeOccupied="timeOccupied"
+      :timeRange="['08:30:00 - 18:00:00']"
+      defaultStart="08:00:00"
+      defaultEnd="18:00:00"
+      @change-time="changeTime"
+    />
+  </div>
+</template>
+
+<script>
+import TimePicker from "./components/TimePicker.vue";
+import TimeRangePicker from "./components/TimeRangePicker.vue";
+import options from "./json/options.json";
+
+export default {
+  name: "app",
+  components: {
+    TimePicker,
+    TimeRangePicker
+  },
+  data() {
+    /***
+    options 全部的时间点
+    timeRange 时间范围
+    timeOccupied 已经占用的时间节点
+    ***/
+    return {
+      defaultStart: "08:30",
+      defaultEnd: "18:00",
+      timeRange: [["08:30", "18:00"]],
+      timeOccupied: [["09:00", "10:00"], ["13:00", "14:30"]],
+      options
+    };
+  },
+  methods: {
+    changeTime() {
+      //获取已经被预定的时间点
+      this.timeOccupied = [["09:00", "10:30"], ["13:00", "14:30"]];
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Microsoft YaHei", Arial, sans-serif;
+  color: #333;
+}
+</style>
---
+++ vue-demo/src/components/TimeRangePicker.vue
@@ -0,0 +1,140 @@
+<template lang="html">
+  <div id="rangeLists">
+    <el-container>
+      <el-main v-cloak>
+        <el-date-picker
+          type="date"
+          :editable="false"
+          @change="dateChange"
+          v-model="date"
+          :picker-options="pickerOptions"
+          value-format="yyyy-MM-dd"
+        ></el-date-picker>
+        <el-time-picker
+          @change="getStartTimeChange"
+          v-model="start"
+          :editable="false"
+          :picker-options="timeSelectStart"
+          value-format="HH:mm"
+          format="HH:mm"
+        ></el-time-picker>
+        <el-time-picker
+          :disabled="timeEndStr"
+          v-model="end"
+          :editable="false"
+          :picker-options="timeSelectEnd"
+          value-format="HH:mm"
+          format="HH:mm"
+        ></el-time-picker>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "time-range-picker",
+  props: {
+    timeOccupied: Array,
+    startRange: {
+      type: Array,
+      default: function() {
+        return ["08:30:00 - 18:00:00"];
+      }
+    },
+    timeRange: Array,
+    defaultStart: { type: String, default: "09:00" },
+    defaultEnd: { type: String, default: "18:00" }
+  },
+  data() {
+    return {
+      date: "",
+      start: "",
+      end: "",
+      timeEndStr: true,
+      timeLists: this.timeOccupied,
+      pickerOptions: {
+        disabledDate(time) {
+          return time.getTime() + 3600 * 1000 * 24 < Date.now();
+        }
+      },
+      timeSelectEnd: {
+        selectableRange: []
+      }
+    };
+  },
+  computed: {
+    timeSelectStart() {
+      const startHour = this.defaultStart.split(":")[0];
+      const endHour = this.defaultEnd.split(":")[0];
+
+      const val = this.timeOccupied;
+      const len = val.length;
+      let timeSelectStart = [];
+      if (len !== 0) {
+        for (let i in val) {
+          const j = Number(i);
+          const x = j + 1;
+          if (j < len - 1) {
+            if (j == 0) {
+              if (val[i][0].split(":")[0] !== startHour) {
+                timeSelectStart.push(
+                  this.defaultStart + " - " + val[i][0] + ":00"
+                );
+              }
+            }
+            if (val[i][1].split(":")[0] !== val[x][0].split(":")[0]) {
+              timeSelectStart.push(val[i][1] + ":00 - " + val[x][0] + ":00");
+            }
+          } else if (j == len - 1) {
+            if (j == 0) {
+              if (val[i][0].split(":")[0] !== startHour) {
+                timeSelectStart.push(
+                  this.defaultStart + " - " + val[i][0] + ":00"
+                );
+              }
+            }
+            timeSelectStart.push(val[i][1] + ":00 - " + this.defaultEnd);
+          }
+        }
+        return { selectableRange: timeSelectStart };
+      } else {
+        return { selectableRange: this.timeRange };
+      }
+    }
+  },
+  methods: {
+    getStartTimeChange(time) {
+      if (this.start != "") {
+        this.timeEndStr = false;
+        let timeSelectEnd = [];
+        let range = this.timeSelectStart.selectableRange;
+        for (let r of range) {
+          let rr = r.split(" - ");
+          let sss = rr[0].split(":");
+          let rrr = rr[1].split(":");
+          let lll = time.split(":");
+          if (lll[0] - sss[0] >= 0 && lll[0] - rrr[0] <= 0) {
+            timeSelectEnd = time + ":00 - " + rr[1];
+          }
+        }
+        this.timeSelectEnd.selectableRange = timeSelectEnd;
+      }
+      this.end = "";
+    },
+    dateChange() {
+      this.start = "";
+      this.end = "";
+      this.timeEndStr = true;
+      this.$emit("change-time");
+    }
+  }
+};
+</script>
+
+<style lang="css" scoped>
+.el-date-editor--time{
+  width: 100px;
+  margin-left: 10px;
+}
+</style>
